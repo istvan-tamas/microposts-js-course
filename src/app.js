9f07f9b47232fab3559b5dc6c4fe5a3155d6ff97
istvan-tamas <istvan.tamas90@gmail.com>
2022-07-10 12:56:52 +0200

    Implemented json server and fixed connection bug with the wrong EasyHTTP

---
+++ src/app.js
@@ -0,0 +1,12 @@
+import {http} from './http';
+import {ui} from './ui';
+
+// get post from DOM load 
+
+document.addEventListener('DOMContentLoaded', getPosts);
+
+function getPosts(){
+    http.get('http://localhost:3000/posts')
+    .then(data => console.log(data))
+    .catch(err => console.log(err));
+}

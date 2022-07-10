8d0578d4c6b65c5154f05efbd9d1fab8f0c17616
istvan-tamas <istvan.tamas90@gmail.com>
2022-07-10 15:41:36 +0200

    UI shows data from the database

--- src/app.js
+++ src/app.js
@@ -7,6 +7,6 @@ document.addEventListener('DOMContentLoaded', getPosts);
 
 function getPosts(){
     http.get('http://localhost:3000/posts')
-    .then(data => console.log(data))
+    .then(data => ui.showPosts(data))
     .catch(err => console.log(err));
 }

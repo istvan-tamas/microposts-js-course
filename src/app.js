22b4752abea467c7d001354622359d5438ef7397
istvan-tamas <istvan.tamas90@gmail.com>
2022-07-10 21:44:56 +0200

    Add post implemented, but the UI wont update needs bug fix

--- src/app.js
+++ src/app.js
@@ -5,6 +5,27 @@ import {ui} from './ui';
 
 document.addEventListener('DOMContentLoaded', getPosts);
 
+document.querySelector('.post-submit',addEventListener("click", submitPost));
+
+function submitPost(){
+    const title = document.querySelector("#title").value;
+    const body = document.querySelector("#body").value;
+
+    const data = {
+        title,
+        body
+    }
+
+    http.post('http://localhost:3000/posts', data)
+    .then(data => {
+        ui.showAlert('Post added', 'alert alert-success');
+        ui.clearFields();
+        getPosts();
+    })
+    .catch(err => console.log(err));
+
+}
+
 function getPosts(){
     http.get('http://localhost:3000/posts')
     .then(data => ui.showPosts(data))
--- src/ui.js
+++ src/ui.js
@@ -14,7 +14,8 @@ class UI {
                 <div class="card mb-3">
                     <div class="card-body">
                         <h4 class="card-title>${post.title}</h4>
-                        <p class="card-text">${post.body}</p>
+                        <p class="card-text">${post.body}
+                        </p>
                         <a href="#" class="edit card-link" data-id="${post.id}"><i class="fa fa-pencil"></i></a>
                         <a href="#" class="delete card-link" data-id="${post.id}"><i class="fa fa-remove"></i></a>
                     </div>
@@ -24,6 +25,40 @@ class UI {
         });
         this.post.innerHTML = output;
     };
+
+    showAlert(message, className) {
+        this.clearAlert();
+
+        const div = document.createElement("div");
+
+        div.className = className;
+
+        div.appendChild(document.createTextNode(message));
+
+        const container = document.querySelector('postsContainer');
+
+        const posts = document.querySelector("#posts");
+
+        container.insertBefore(div, posts)
+
+
+        setTimeout(() => {
+            this.clearAlert();
+        }, 3000);
+    }
+
+    clearAlert(){
+        const currentAlert = document.querySelector('.alert');
+        if(currentAlert){
+            currentAlert.remove();
+        }
+
+    }
+    clearFields(){
+        this.titleInput.value = "";
+        this.bodyInput.value = "";
+    }
+
 }
 
 export const ui = new UI();

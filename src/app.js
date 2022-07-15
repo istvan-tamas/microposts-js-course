23d5a854f909d6fc84f71c49d86f7b5a4b709da5
istvan-tamas <istvan.tamas90@gmail.com>
2022-07-15 14:55:27 +0200

    Finished the project

--- src/app.js
+++ src/app.js
@@ -9,6 +9,10 @@ document.querySelector('.post-submit').addEventListener("click", submitPost);
 
 document.querySelector('#posts').addEventListener("click", deletePost);
 
+document.querySelector('#posts').addEventListener("click", enableEdit);
+
+document.querySelector('.card-form').addEventListener('click',cancelEdit);
+
 function getPosts(){
     http.get('http://localhost:3000/posts')
     .then(data => ui.showPosts(data))
@@ -19,19 +23,35 @@ function getPosts(){
 function submitPost(){
     const title = document.querySelector("#title").value;
     const body = document.querySelector("#body").value;
+    const id = document.querySelector('#id').value;
 
     const data = {
         title,
         body
     }
 
-    http.post('http://localhost:3000/posts', data)
-    .then(data => {
-        ui.showAlert('Post added', 'alert alert-success');
-        ui.clearFields();
-        getPosts();
-    })
-    .catch(err => console.log(err));
+
+    if(title === '' || body === ''){
+        showAlert('Please fill in all fields', 'alert alert-danger');
+    }else{
+        if(id === ''){
+            http.post('http://localhost:3000/posts', data)
+            .then(data => {
+                ui.showAlert('Post added', 'alert alert-success');
+                ui.clearFields();
+                getPosts();
+            })
+            .catch(err => console.log(err));
+        }else{
+            http.put(`http://localhost:3000/posts/${id}`, data)
+            .then(data => {
+                ui.showAlert('Post added', 'alert alert-success');
+                ui.changeFormState('add');
+                getPosts();
+            })
+            .catch(err => console.log(err));
+        } 
+    }
 
 }
 
@@ -49,4 +69,30 @@ function deletePost(e){
             .catch(err => console.log(err));
         }
     }
+}
+
+function enableEdit(e){
+    if(e.target.parentElement.classList.contains('edit')){
+        const id = e.target.parentElement.dataset.id;
+        const body = e.target.parentElement.previousElementSibling.textContent;
+        const title = e.target.parentElement.previousElementSibling.previousElementSibling.textContent;
+        const data = {
+            id,
+            title,
+            body
+        }
+
+
+        ui.fillForm(data);
+    }
+
+    e.preventDefault();
+}
+
+function cancelEdit(){
+    if(e.target.classList.contains('post-cancel')){
+        ui.changeFormState('add');
+
+    }
+    e.preventDefault();
 }
